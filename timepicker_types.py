81a24b1e4793d4ca9cee0a8cf4cf4d487b4cc853
Koma <aleksandr.orlichenya@gmail.com>
2023-07-15 11:50:29 +0300

    create carousel timpicker classes

--- timepicker_types.py
+++ timepicker_types.py
@@ -24,3 +24,34 @@ class FullTimePickerCallback(CallbackData, prefix='full_timepicker'):
     hour: int
     minute: int
     second: int
+
+
+class CarouselTimePickerAction(IntEnum):
+    INCREASE_HOUR_BY_5 = 0
+    INCREASE_HOUR_BY_1 = 1
+    DECREASE_HOUR_BY_1 = 2
+    DECREASE_HOUR_BY_5 = 3
+    INCREASE_MINUTE_BY_5 = 4
+    INCREASE_MINUTE_BY_1 = 5
+    DECREASE_MINUTE_BY_1 = 6
+    DECREASE_MINUTE_BY_5 = 7
+    INCREASE_SECOND_BY_5 = 8
+    INCREASE_SECOND_BY_1 = 9
+    DECREASE_SECOND_BY_1 = 10
+    DECREASE_SECOND_BY_5 = 11
+    SELECT_HOUR = 12
+    SELECT_MINUTE = 13
+    SELECT_SECOND = 14
+    SELECTED_HOUR = 15
+    SELECTED_MINUTE = 16
+    SELECTED_SECOND = 17
+    CANCEL = 18
+    CONFIRM = 19
+    IGNORE = 20
+
+
+class CarouselTimePickerCallback(CallbackData, prefix="carousel_timepicker"):
+    act: CarouselTimePickerAction
+    hour: int
+    minute: int
+    second: int
